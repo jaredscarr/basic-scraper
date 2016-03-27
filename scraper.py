e866ffe0993533f12b710a0a16204641bc32cb54
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-27 09:50:21 -0700

    old scraper removed. Scaper2 renamed to scraper.py. open_file removedbecause no longer needed.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 import io
+import re
 import requests
-import sys
 from bs4 import BeautifulSoup
+import sys
 
 
 DOMAIN = 'http://info.kingcounty.gov'
@@ -26,37 +26,122 @@ PARAMS = {
     'Sort': 'H'
 }
 
+TEST_HTML = 'inspection_page.html'
+
 
 def get_inspection_page(**kwargs):
-    """Retrieve the content and encoding from request."""
     url = DOMAIN + PATH
-    params = PARAMS
+    params = PARAMS.copy()
     for key, val in kwargs.items():
         if key in PARAMS:
             params[key] = val
-    response = requests.get(url, params=params)
-    response.raise_for_status() # <- This is a no-op if there is no HTTP error
+    resp = requests.get(url, params=params)
+    resp.raise_for_status() # <- This is a no-op if there is no HTTP error
     # remember, in requests `content` is bytes and `text` is unicode
-    return response.content, response.encoding
+    return resp.content, resp.encoding
 
 
-def load_inspection_page():
-    """Read file from disk and return content and encoding."""
-    open_file = io.open('inspection_page.html')
-    file = open_file.read()
-    open_file.close()
-    return file
-
-
-def parse_source(html):
-    parsed = BeautifulSoup(html.encode(), 'html5lib')
+def parse_source(html, encoding='utf-8'):
+    if isinstance(html, bytes):
+        parsed = BeautifulSoup(html, 'html5lib', from_encoding=encoding)
+    else:
+        parsed = BeautifulSoup(html, 'html5lib')
     return parsed
 
 
-def extract_data_listings(parsedhtml):
-    """Filter the parsed html."""
-    scraped = parsedhtml.find_all('p')
-    return scraped
+def load_inspection_page(file):
+    open_file = io.open(file)
+    file = open_file.read()
+    open_file.close()
+    return file, 'utf-8'
+
+
+def extract_data_listings(html):
+    id_finder = re.compile(r'PR[\d]+~')
+    return html.find_all('div', id=id_finder)
+
+
+def has_two_tds(elem):
+    is_tr = elem.name == 'tr'
+    td_children = elem.find_all('td', recursive=False)
+    has_two = len(td_children) == 2
+    return is_tr and has_two
+
+
+def clean_data(td):
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
+
+
+def extract_restaurant_metadata(elem):
+    metadata_rows = elem.find('tbody').find_all(
+        has_two_tds, recursive=False
+    )
+    rdata = {}
+    current_label = ''
+    for row in metadata_rows:
+        key_cell, val_cell = row.find_all('td', recursive=False)
+        new_label = clean_data(key_cell)
+        current_label = new_label if new_label else current_label
+        rdata.setdefault(current_label, []).append(clean_data(val_cell))
+    return rdata
+
+
+def is_inspection_row(elem):
+    is_tr = elem.name == 'tr'
+    if not is_tr:
+        return False
+    td_children = elem.find_all('td', recursive=False)
+    has_four = len(td_children) == 4
+    this_text = clean_data(td_children[0]).lower()
+    contains_word = 'inspection' in this_text
+    does_not_start = not this_text.startswith('inspection')
+    return is_tr and has_four and contains_word and does_not_start
+
+
+def extract_score_data(elem):
+    inspection_rows = elem.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total/float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
+
+def generate_results(test=False):
+    kwargs = {
+        'Inspection_Start': '2/1/2013',
+        'Inspection_End': '2/1/2015',
+        'Zip_Code': '98109'
+    }
+    if test:
+        html, encoding = load_inspection_page('inspection_page.html')
+    else:
+        html, encoding = get_inspection_page(**kwargs)
+    doc = parse_source(html, encoding)
+    listings = extract_data_listings(doc)
+    for listing in listings:
+        metadata = extract_restaurant_metadata(listing)
+        score_data = extract_score_data(listing)
+        metadata.update(score_data)
+        yield metadata
 
 
 if __name__ == '__main__':
@@ -65,17 +150,13 @@ if __name__ == '__main__':
         'Inspection_End': '2/1/2015',
         'Zip_Code': '98109'
     }
-
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        # you will likely have something different here, depending on how
-        # you implemented the load_inspection_page function.
-        # html = load_inspection_page()
-        html, encoding = load_inspection_page('inspection_page.html')
+        html, encoding = load_inspection_page(TEST_HTML)
     else:
         html, encoding = get_inspection_page(**kwargs)
-    doc = parse_source(html)
-    extracted = extract_data_listings(doc)
-    print(extracted)
-    # doc = parse_source(html, encoding)
-    # print doc.prettify(encoding=encoding)
-
+    doc = parse_source(html, encoding)
+    listings = extract_data_listings(doc)
+    for listing in listings:
+        metadata = extract_restaurant_metadata(listing)
+        score_data = extract_score_data(listing)
+        print(score_data)
